3794673146d9375a44b0a8dde135571d34f014dc
Nils <80416356+nils-dambowy@users.noreply.github.com>
2024-06-30 06:24:54 +0200

    learned basic syntax

--- sandbox.js
+++ sandbox.js
@@ -292,3 +292,177 @@ console.log(Math.round(Math.PI));
 
 console.log(Math.random());
 */
+
+// Window object
+
+/* document object model
+- created by the browser
+- interact with the html object
+
+
+// query selector
+let ul = document.querySelector(".people");
+console.log(ul);
+ul.innerHTML= "<li>Test HTML! :)</li>";
+
+let para = document.querySelector("p");
+para.innerHTML = "This is a paragraph!";
+
+let para2 = document.querySelector(".error");
+para2.innerHTML = "This is a error message!";
+
+let error2 = document.querySelector("div.error");
+error2.innerHTML = "2nd error message!";
+
+// gets us the node list
+let allQuery = document.querySelectorAll("p");
+for (let i = 0; i < allQuery.length; i++) {
+    allQuery[i].innerHTML = "did this work?";
+}
+
+allQuery.forEach(element => {
+    element.innerHTML = "back";
+});
+
+
+// get element by an ide
+const title = document.getElementById("page-title");
+
+// get element by their class name
+const errors = document.getElementsByClassName("error");
+
+// we can not use the foreach method on an html collection
+console.log(errors);
+console.log(errors[0]);
+
+const paras = document.getElementsByTagName("p");
+console.log(paras);
+
+
+// first p of the bage
+const paras = document.querySelectorAll("p");
+paras.forEach(para => {
+    console.log(para.innerText);
+    para.innerText += " xD";
+})
+
+const content = document.querySelector(".content");
+//console.log(content.innerHTML);
+content.innerHTML = "<h2> This a new h2! </h2>";
+
+const people = ["a", "b", "c"];
+
+people.forEach(person => {
+    content.innerHTML += `<p>${person}</p>`;
+})
+
+
+const link = document.querySelector("a");
+console.log(link.getAttribute("href"));
+link.setAttribute("href", "https://www.google.com/maps");
+link.innerText = "Maps";
+
+const mssg = document.querySelector("p");
+console.log(mssg.getAttribute("class"));
+mssg.setAttribute("class", "sucess");
+mssg.setAttribute("style", "color:green");
+
+
+const title = document.querySelector("h1");
+title.style.margin  ="50px";
+title.style.color = "orange";
+title.style.fontFamily = "Satoshi";
+title.style.fontSize = "70px";
+
+const content = document.querySelector("p");
+content.classList.add("error");
+content.classList.remove("error");
+content.classList.add("success");
+
+const article = document.querySelector("article");
+Array.from(article.children).forEach( child => {
+    child.classList.add("article-element")
+})
+
+const title2 = document.querySelector("h2");
+console.log(title2.parentElement);
+
+const ul = document.querySelector("ul")
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    const li = document.createElement("li");
+    li.textContent = "something...";
+    ul.append(li);
+
+});
+
+ul.addEventListener("click", e => {
+    if (e.target.tagName === "LI") {
+        e.target.remove();
+    }
+});
+
+const copy = document.querySelector(".copy-me");
+copy.addEventListener("copy", () =>{
+    console.log("copied!");
+})
+
+const box = document.querySelector(".box");
+box.addEventListener("mousemove", (event) =>{
+    box.textContent = `pos x: ${event.offsetX} pos y: ${event.offsetY}`;
+})
+
+document.addEventListener('wheel', e => {
+    console.log(e.pageX, e.pageY);
+})
+*/
+const button = document.querySelector('button');
+const popup = document.querySelector(".popup-wrapper");
+const close_button = document.querySelector('.popup-close');
+const e_wrapper = document.querySelector(".popup-wrapper");
+
+button.addEventListener("click", e =>{
+    popup.style.display ="block";
+})
+
+close_button.addEventListener("click", e =>{
+    popup.style.display = "none";
+})
+
+e_wrapper.addEventListener("click", e =>{
+    popup.style.display = "none";
+})
+
+const form = document.querySelector(".signup-form");
+//const username = document.querySelector("#username");
+const pattern  = /^[a-z]{6,}$/;
+
+//const fb_div = document.querySelector("#feedback");
+const fb_div = document.getElementById("#feedback");
+
+
+form.addEventListener("submit",  e => {
+    e.preventDefault();
+    const username = form.username.value;
+    if(pattern.test(username)){
+        console.log("username correct!");
+        fb_div.setAttribute("class", "success");
+        fb_div.innerHTML ="CORRECT INPUT"
+    }
+    else{
+        console.log("invalid!");
+        fb_div.setAttribute("class", "error");
+        fb_div.innerHTML ="WRONG INPUT"
+    }
+})
+
+form.username.addEventListener("keyup", e =>{
+    //console.log(e.target.value, form.username.value);
+    if(pattern.test(e.target.value)){
+        form.username.setAttribute("class", "form_success");
+    }
+    else{
+        form.username.setAttribute("class", "form_error");
+    }
+})
